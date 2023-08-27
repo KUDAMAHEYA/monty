1f8deb51efd412e9839addca7d6edaf4bb2cf200
KUDAMAHEYA <Milaceymilacey@gmail.com>
2023-08-27 14:48:07 -0700

    0x19. C - Stacks, Queues - LIFO, FIFO

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- nop
+  *@head: stack head
+  *@counter: line_enteger
+  *Return: nothing
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
